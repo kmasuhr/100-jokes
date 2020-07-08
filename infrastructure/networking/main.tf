9dd332ffa2f3a3b58d7a2b4061730f1b748d461a
Karol Masuhr <karol@masuhr.pl>
2020-07-08 14:24:00 +0200

    Move terraform output to seperate file

--- infrastructure/networking/main.tf
+++ infrastructure/networking/main.tf
@@ -25,7 +25,3 @@ resource "aws_route_table_association" "main" {
   subnet_id = aws_subnet.main.id
   route_table_id = aws_route_table.main.id
 }
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
