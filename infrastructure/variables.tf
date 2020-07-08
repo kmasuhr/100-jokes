7edafcd96d1aeb1755a62910363bb25f6cc8fedb
Karol Masuhr <karol@masuhr.pl>
2020-07-08 14:12:26 +0200

    Add draft of terraform infra project

---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+variable "cidr_vpc" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "cidr_subnet" {
+  type = string
+  default = "10.0.0.0/24"
+}
+
+variable "public_key_path" {
+  type = string
+  default = "~/.ssh/scalac/id_rsa.pub"
+}
