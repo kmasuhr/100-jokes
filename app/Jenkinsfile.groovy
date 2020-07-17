139a2d0c39e3cc24b6591d1f05fa8e3bad3ff3ea
Karol Masuhr <karol@masuhr.pl>
2020-07-17 08:30:43 +0200

    Add deployment stage

--- app/Jenkinsfile.groovy
+++ app/Jenkinsfile.groovy
@@ -11,5 +11,12 @@ pipeline {
         sh "docker build -t 100-jokes:0.1.${env.BUILD_NUMBER} -f ./app/Dockerfile ./app"
       }
     }
+
+    stage('Deploy') {
+      steps {
+        sh "docker kill app"
+        sh "docker run -p 3000:3000 --name app 100-jokes:0.1.${env.BUILD_NUMBER}"
+      }
+    }
   }
 }
